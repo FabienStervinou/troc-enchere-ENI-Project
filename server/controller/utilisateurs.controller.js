e299d0af5d73ab41a008e204b9dd37a432537b3d
FabienStervinou <stervinoufabien@gmail.com>
2020-09-08 18:23:03 +0200

    CREATE controller folder AND ADD utilisateurs controller implemente create method

---
+++ server/controller/utilisateurs.controller.js
@@ -0,0 +1,49 @@
+const db = require("../models")
+const utilisateurs = db.utilisateurs
+const Op = db.Sequelize.Op
+
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body.title) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
+
+  // Create a Tutorial
+  const tutorial = {
+    title: req.body.title,
+    description: req.body.description,
+    published: req.body.published ? req.body.published : false
+  };
+
+  // Save Tutorial in the database
+  Tutorial.create(tutorial)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while creating the Tutorial."
+      });
+    });
+};
+
+exports.findAll = (req, res) => {
+
+};
+
+exports.findOne = (req, res) => {
+
+};
+
+exports.update = (req, res) => {
+
+};
+
+exports.delete = (req, res) => {
+
+};
+
+// exports.deleteAll = (req, res) => {};
