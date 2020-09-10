094cf1b5d4735441460383add3f9f5cc06e4bba1
FabienStervinou <stervinoufabien@gmail.com>
2020-09-10 19:07:24 +0200

    Database config update

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   HOST: process.env.DB_HOST,
-  USER: process.env.DB_USER,
-  PASSWORD: process.env.DE_PASSWORD,
+  USER: process.env.DB_USERNAME,
+  PASSWORD: process.env.DB_PASSWORD,
   DB: process.env.DB_NAME,
   dialect: 'mysql',
   pool: {
@@ -9,5 +9,8 @@ module.exports = {
     min: 0,
     acquire: 30000,
     idle: 10000
+  },
+  define: {
+    timestamps: false
   }
 };
