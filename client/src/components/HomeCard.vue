beafc057d83bb254d222096a258847dd5c6aa17e
FabienStervinou <stervinoufabien@gmail.com>
2020-09-13 11:42:28 +0200

    add component HomeCard HomeFilter NavBar

---
+++ client/src/components/HomeCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card-container flex-row">
+    <div v-for="item in items" :key="item.id" class="card flex-">
+      {{ item.id }}
+      {{ item.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.card-container {
+  padding: 0 125px;
+  flex-wrap: wrap;
+  justify-content: space-around;
+
+  .card {
+    background-color: rgb(255, 185, 185);
+    height: 200px;
+    width: calc(100% / 2 - 100px);
+    margin-bottom: 90px;
+  }
+}
+</style>
---
+++ client/src/components/HomeFilter.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container-filter">
+    <form action="#" method="GET" class="form-filter flex-row">
+      <div class="half-w flex-col">
+        <label for="search" class="label-filter">
+          Filtres :
+          <input
+            type="text"
+            name="search"
+            placeholder="Nom de l'article contient"
+          />
+        </label>
+        <label for="categories" class="filter-label">
+          Catégorie :
+          <input name="categorie" list="categorie" />
+          <datalist id="categorie">
+            <option value="Mobilier" />
+            <option value="Informatique" />
+            <option value="Téléphonie" />
+            <option value="Mode" />
+            <option value="Jeux vidéo" />
+          </datalist>
+        </label>
+      </div>
+      <div class="half-w">
+        <input type="submit" value="Rechercher" class="btn btn-search" />
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.container-filter {
+  width: calc(100% - 250px);
+  padding: 50px 125px 0 125px;
+
+  .form-filter {
+    width: 100%;
+
+    .label-filter {
+      padding: 25px 0;
+    }
+
+    .half-w {
+      height: 150px;
+
+      .btn-search {
+        padding: 50px 150px;
+      }
+    }
+  }
+}
+</style>
