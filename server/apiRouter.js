619e4ea6cc5d96de5dcf0f2909df2f1a4dc179c8
FabienStervinou <stervinoufabien@gmail.com>
2020-09-25 15:29:30 +0200

    Add api router

---
+++ server/apiRouter.js
@@ -0,0 +1,14 @@
+// Imports
+const express = require('express');
+const usersCtrl = require('./routes/usersController');
+
+// Router
+exports.router = (function () {
+  var apiRouter = express.Router();
+
+  // Users routes
+  apiRouter.route('/users/register/').post(usersCtrl.register);
+  apiRouter.route('/users/login/').post(usersCtrl.login);
+
+  return apiRouter;
+})();
