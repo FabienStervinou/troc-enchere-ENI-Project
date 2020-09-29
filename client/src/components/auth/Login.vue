ac1b4ac9aaf4e48449f2bb19135f93e25eb1b2f4
FabienStervinou <stervinoufabien@gmail.com>
2020-09-29 22:40:37 +0200

    Create Login component

---
+++ client/src/components/auth/Login.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="login-form flex-center">
+    <form 
+      class="login" 
+      @submit.prevent="login">
+      <section>
+        <div class="flex-between">
+          <label for="email">Identifiant</label>
+          <input
+            required
+            v-model="email"
+            type="text"
+            name="email"
+          >
+        </div>
+        <div class="flex-between">
+          <label for="password">Mot de passe</label>
+          <input
+            required
+            v-model="password"
+            type="password"
+            name="password" 
+            autocomplete="on"
+          >
+        </div>
+      </section>
+      <section class="flex-row">
+        <div class="half-w">
+          <button 
+            class="btn btn-login"
+            type="submit"
+            >
+            Connexion
+          </button>
+        </div>
+        <div class="half-w flex-col">
+          <label for="remember-me">
+            <input
+              type="checkbox"
+              id="remember-me"
+              name="remember-me"
+            >
+            Se Souvenir de moi
+          </label>
+          <a
+            href="#"
+            class="remember-me"
+          >Mot de passe oublié</a>
+        </div>
+      </section>
+      <section>
+        <button class="btn btn-register">
+          Créer un compte
+        </button>
+      </section>
+    </form>
+  </div>
+</template>
+
+<script>
+export default { 
+  name: 'login',
+  data() {
+    return {
+        id: null,
+        email: '',
+        password: '',
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.login-form {
+  form {
+    width: 350px;
+
+    section {
+      min-height: 75px;
+      margin-top: 30px;
+
+      .flex-between {
+        margin-bottom: 15px;
+
+        label {
+          justify-content: space-between;
+        }
+      }
+    }
+  }
+}
+
+.btn-login {
+  padding: 25px 40px;
+}
+
+.remember-me {
+  text-decoration: underline;
+
+  &:hover {
+    color: #51ca98;
+  }
+}
+
+.btn-register {
+  padding: 50px 108px;
+}
+</style>
