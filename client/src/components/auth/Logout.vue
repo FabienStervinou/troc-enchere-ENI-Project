6e657c45c5546fbbfff15097a5f165721d09e872
FabienStervinou <stervinoufabien@gmail.com>
2020-09-29 22:49:22 +0200

    Create logout component

---
+++ client/src/components/auth/Logout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Déconnexion...
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    this.$store.dispatch('destroyToken')
+    .then(response => {
+      this.$router.push({ name: 'home' })
+      return response
+    })
+  }
+}
+</script>
