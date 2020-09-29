2dcb86e9ffb200ba3f8e0e87ed444137fad67616
FabienStervinou <stervinoufabien@gmail.com>
2020-09-29 23:06:17 +0200

    Create router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,36 +1,47 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-import Login from "../components/Login.vue";
-import User from "../components/User.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Login from '../components/auth/Login.vue';
+import Logout from '../components/auth/Logout.vue';
+import Register from '../components/auth/Register.vue';
+import User from '../components/User.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "root",
+    path: '/',
+    name: 'home',
     component: Home
   },
   {
-    path: "/login",
-    name: "login",
+    path: '/login',
+    name: 'login',
     component: Login
   },
   {
-    path: "user:id",
-    name: "user",
+    path: '/logout',
+    name: 'logout',
+    component: Logout
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+  {
+    path: '/user:id',
+    name: 'user',
     component: User
   },
   {
-    path: "*",
-    redirect: "/"
+    path: '*',
+    redirect: '/'
   }
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
+  mode: 'history',
   routes
 });
 
