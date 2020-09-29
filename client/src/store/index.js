f4142ab7dd1dd43d4a0e89b9497be53e50da6124
FabienStervinou <stervinoufabien@gmail.com>
2020-09-29 23:04:29 +0200

    Create vue store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,11 +1,50 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios'
 
 Vue.use(Vuex);
 
+axios.defaults.baseURL = 'http://localhost:3000/api';
+
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    token: localStorage.getItem('access_token') || null,
+    users: []
+  },
+  mutations: {
+    retrieveToken(state, token) {
+      state.token = token
+    },
+    destroyToken(state) {
+      state.token = null  
+    },
+  },
+  actions: {
+    retrieveToken(context, credentials) {
+      return new Promise((resolve, reject) => {
+        axios.post('/users/login', {
+          email: credentials.email,
+          password: credentials.password
+        })
+          .then(response => {
+            const token = response.data.token
+
+            localStorage.setItem('access_token', token)
+            context.commit('retrieveToken', token)
+            resolve(response)
+          })
+          .catch(err => {
+            console.log(err)
+            reject(err)
+          })
+      })
+    }
+  },
+  getters: {
+    loggedIn(state) {
+      return state.token !== null
+    },
+  },
   modules: {}
 });
