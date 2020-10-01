3015708176fa7b7b3df6c347135c456b97150715
FabienStervinou <stervinoufabien@gmail.com>
2020-10-01 17:14:41 +0200

    Update model User and db migration

--- server/migrations/20200924151742-create-user.js
+++ server/migrations/20200924151742-create-user.js
@@ -1,4 +1,3 @@
-'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Users', {
@@ -6,7 +5,7 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       username: {
         allowNull: false,
