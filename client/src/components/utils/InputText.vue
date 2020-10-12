6f934d2b6126a63826dc8d960e62a211ebabf6ad
FabienStervinou <stervinoufabien@gmail.com>
2020-10-12 11:50:51 +0200

    Component InputText debug

--- client/src/components/utils/InputText.vue
+++ client/src/components/utils/InputText.vue
@@ -4,16 +4,10 @@
       {{ label }}
     </label>
     <input 
-<<<<<<< HEAD
       v-bind="$attrs"
       :type="type"
       :value="value"
       @input="$emit('input', $event.target.value)"
-=======
-    
-      :value="value"
-      @input="$emit('update', $event.target.value)"
->>>>>>> 8829c99d957b43281a71d2d4c73b7f4c704532d6
     >
   </div>
 </template>
@@ -21,7 +15,6 @@
 <script>
 
 const TYPES = [
-<<<<<<< HEAD
   'text',
   'password', 
   'email',
@@ -33,26 +26,12 @@ const TYPES = [
   'date',
   'image',
   'file'
-=======
-  'text', 
-  'password', 
-  'email', 
-  'number', 
-  'url', 
-  'tel', 
-  'search',  
-  'color'
->>>>>>> 8829c99d957b43281a71d2d4c73b7f4c704532d6
 ]
 
 const includes = types => type => types.includes(type);
 
 export default {
   name: 'InputText',
-<<<<<<< HEAD
-=======
-  inheritAttrs: false,
->>>>>>> 8829c99d957b43281a71d2d4c73b7f4c704532d6
   props: {
     label: {
       type: String,
@@ -73,23 +52,11 @@ export default {
         return isValid
       },
     },
-<<<<<<< HEAD
   },
 }
 </script>
 
 <style lang='scss' scoped>
-=======
-},
-  model: {
-    prop: 'value',
-    event: 'update'
-  }
-}
-</script>
-
-<style lang=scss scoped>
->>>>>>> 8829c99d957b43281a71d2d4c73b7f4c704532d6
   .form-item {
     background-color: #f1f1f1;
     margin-bottom: 15px;
@@ -106,30 +73,18 @@ export default {
     }
 
     input {
-<<<<<<< HEAD
       height: calc(100% - 18px);
       width: calc(100% - 14px);
       background-color: transparent;
       position: relative;
       padding: 14px 0 0 10px;
-=======
-      height: calc(100% - 14px);
-      width: calc(100% - 14px);
-      background-color: transparent;
-      position: relative;
-      padding: 10px 0 0 10px;
->>>>>>> 8829c99d957b43281a71d2d4c73b7f4c704532d6
       font-size: 1.3em;
       border: solid #e9e9e9 2px;
       border-radius: 5px;
 
       &:hover {
-<<<<<<< HEAD
         cursor: pointer;
         border: solid $primary_color_lighter 2px;
-=======
-        cursor: default;
->>>>>>> 8829c99d957b43281a71d2d4c73b7f4c704532d6
       }
 
       &:focus {
